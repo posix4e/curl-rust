35c2a43975b15888383a416d22b6e6ac6c9b764d
Alex Newman <posix4e@gmail.com>
2015-06-08 22:37:41 -0700

    bug report

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,13 +1,21 @@
 pub use self::handle::{Handle,Request};
+pub use self::multi_handle::{MultiHandle,Request};
+
 pub use self::response::{Headers,Response};
 
 pub mod body;
 pub mod handle;
-pub mod multi_handler;
+pub mod multi_handle;
 pub mod header;
 mod response;
 
 #[inline]
 pub fn handle() -> Handle {
     Handle::new()
+}
+
+
+#[inline]
+pub fn multi_handle() -> MultiHandle {
+    MultiHandle::new()
 }
